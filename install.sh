3ec483e1aa9cf36ff6d6acb3d71c8d33d100e131
Korvag <bptoner123@gmail.com>
2024-01-11 17:54:59 -0500

    {new files}

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+#installers
+sudo pacman -S meson ninja cmake scdoc pkgconf git
+
+#dependencies
+sudo pacman -S gtkmm3 jsoncpp libsigc++ fmt wayland wayland-protocols chrono-date spdlog libgtk-3-dev gobject-introspection libgirepository1.0-dev libpulse libn1 libappindicator-gtk3 libdbusmenu-gtk3 libmpdclient libsndio libevdev xkbregistry upower pango cairo file libglvnd libglvnd-core libjpeg-turbo libwebp hyprlang gcc
+
+#yay
+git clone https://aur.archlinux.org/yay-bin
+cd yay-bin
+makepkg -si
+cd
+
+#packages
+sudo pacman -S hyprpaper neofetch btop waybar
+
+git clone https://github.com/Korvag/wallpapers.git
+git clone https://aur.archlinux.org/sddm-sugar-candy-git.git
+
+
+cd sddm-sugar-candy-git
+makepkg -si
+cd
+
+
+sudo cp ~/wallpapers/tree.jpg /usr/share/sddm/themes/sugar-candy/Backgrounds/
+sudo cp ~/archpkgs/theme.conf /usr/share/sddm/themes/sugar-candy/
+sudo cp ~/archpkgs/default.conf /usr/lib/sddm/sddm.conf.d/
+sudo cp ~/archpkgs/hyprpaper.conf ~/.config/hypr/
+sudo cp ~/archpkgs/hyprland.conf ~/.config/hypr/
+sudo cp ~/archpkgs/btop.sh ~/.config/hypr
+sudo cp ~/archpkgs/waybar.conf /usr/local/etc/xdg/waybar/config
+sudo cp ~/archpkgs/style.css /usr/local/etc/xdg/waybar/
+
+
+cd
+chmod +x ~/.config/hypr/btop.sh
