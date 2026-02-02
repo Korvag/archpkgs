c52a32c644432c779898ed169df89249e3685fd1
Korvag <btonerfranciscan@gmail.com>
2026-02-01 22:50:21 -0500

    update test.sh

--- test.sh
+++ test.sh
@@ -2,15 +2,6 @@
 #!/bin/bash
 
 git clone https://aur.archlinux.org/yay-bin.git
-
-#installers
-#sudo pacman -S meson ninja cmake scdoc pkgconf base_devel --noconfirm
-
-#dependencies
-#sudo pacman -S jsoncpp libsigc++ fmt wayland wayland-protocols chrono-date spdlog gtk3 gobject-introspection \
-#    libpulse libappindicator-gtk3 libdbusmenu-gtk3 libmpdclient libevdev upower pango cairo file libglvnd libjpeg-turbo libwebp hyprlang gcc pamixer \
-#    libnl sndio --noconfirm
-
 sudo pacman -S base_devel
 
 #packages
@@ -18,23 +9,17 @@ sudo pacman -S fastfetch btop thunar --noconfirm
 
 sudo pacman -S python --noconfirm
 sudo pacman -S python-pip --noconfirm
-sudo pacman -S python-packaging --noconfirm
+#sudo pacman -S python-packaging --noconfirm
 
 #build packages
 cd yay-bin
 makepkg -srci --noconfirm
 
 sudo pacman -S hyprpaper
-
-#cd ~/sddm-sugar-dark
-#makepkg pkgbuild -srci --noconfirm
-#cd
 sudo yay -S sddm-sugar-dark
 
 mkdir wallpapers
 mkdir wallpapers/active
-#sudo mkdir /usr/local/etc/xdg/
-#sudo mkdir /usr/local/etc/xdg/waybar
 cd
 
 #set up configs
@@ -46,9 +31,6 @@ sudo cp ~/archpkgs/default.conf /usr/lib/sddm/sddm.conf.d/
 sudo cp ~/archpkgs/hyprpaper.conf ~/.config/hypr/
 sudo cp ~/archpkgs/hyprland.conf ~/.config/hypr/
 sudo cp ~/archpkgs/btop.sh ~/.config/hypr
-#sudo cp ~/archpkgs/waybar.conf /usr/local/etc/xdg/waybar/waybar.conf
-#sudo mv /usr/local/etc/xdg/waybar/waybar.conf /usr/local/etc/xdg/waybar/config
-#sudo cp -f ~/archpkgs/style.css /usr/local/etc/xdg/waybar/style.css
 sudo cp ~/archpkgs/.bashrc ~/
 sudo cp ~/archpkgs/.nanorc ~/
 sudo cp ~/archpkgs/kitty.conf ~/.config/kitty/
